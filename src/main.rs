91767609fa2e706c82448f187ea010ccc68fcfd0
Akshay Gupta <kitallis@gmail.com>
2019-09-02 22:26:15 +0530

    Refactor a whole bunch of transformation functions

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,11 @@ use ansi_term::Colour::Green;
 
 use maps::{HIRAGANA_TO_KATAKANA, ROMAJI_TO_KANA, DOUBLE_CONSONANTS_TO_KANA};
 
-//
 // hiragana + katakana char list
-//
 static KANA_BEG: char = '\u{3040}';
 static KANA_END: char = '\u{30FF}';
 
-//
 // full-width roman char list
-//
 static FULL_WIDTH_ROMAN_BEG: char = '\u{0021}';
 static FULL_WIDTH_ROMAN_END: char = '\u{007E}';
 
@@ -41,58 +37,61 @@ fn main() {
     println!("{}", "🍙")
 }
 
-fn to_kana(s: &str, katakana: bool) {
-    let hiragana_output = replace_by_size(&to_double_consonants(&s));
+fn to_kana(s: &str, enable_katakana: bool) {
+    let hiragana_output = romaji_to_kana(&to_double_consonants(&s));
     let mut katakana_output = String::new();
 
-    if katakana {
-        katakana_output = to_katakana(&hiragana_output);
-    }
+    if enable_katakana { katakana_output = to_katakana(&hiragana_output); }
 
     println!("hiragana: {}\nkatakana: {}", hiragana_output, katakana_output);
 }
 
 fn is_fully_kana(s: &str) -> bool {
-    for ch in s.trim().chars() {
-        if !(ch >= KANA_BEG && ch <= KANA_END) { return false; }
-    }
-
-    true
+    is_str_between_char_range(s, KANA_BEG, KANA_END)
 }
 
 fn is_fully_romaji(s: &str) -> bool {
+    is_str_between_char_range(s, FULL_WIDTH_ROMAN_BEG, FULL_WIDTH_ROMAN_END)
+}
+
+fn is_str_between_char_range(s: &str, range_beg: char, range_end: char) -> bool {
     for ch in s.trim().chars() {
-        if !(ch >= FULL_WIDTH_ROMAN_BEG && ch <= FULL_WIDTH_ROMAN_END) { return false; }
+        if !(ch >= range_beg && ch <= range_end) { return false; }
     }
 
     true
 }
 
 fn to_double_consonants(s: &str) -> String {
-    let mut result = String::from(s);
-
-    for (k, v) in DOUBLE_CONSONANTS_TO_KANA.iter() {
-        result = result.replace(k, v);
-    }
-
-    result
+    repeatedly_replace_str_with_map(&s, &DOUBLE_CONSONANTS_TO_KANA)
 }
 
 fn to_katakana(s: &str) -> String {
-    let mut result = String::from(s);
-
-    for (k, v) in HIRAGANA_TO_KATAKANA.iter() {
-        result = result.replace(k, v);
-    }
-
-    result
+    repeatedly_replace_str_with_map(&s, &HIRAGANA_TO_KATAKANA)
 }
 
-fn replace_by_size(s: &str) -> String {
+fn repeatedly_replace_str_with_map(s: &str, map: &HashMap<&str, &str>) -> String {
+    map.iter().fold(String::from(s), |mut acc, (k, v)| {
+        acc = acc.replace(k, v);
+        acc
+    })
+}
+
+fn romaji_to_kana(s: &str) -> String {
+    // BTreeMap to allow for a sorted lookup by key length
+    let mut romaji_to_kana_by_key_size: BTreeMap<usize, HashMap<&str, &str>> = BTreeMap::new();
     let mut result = String::from(s);
 
-    // reversing the BTreeMap iterator to effectively start from the largest romaji key
-    for (_k, v) in romaji_to_kana().iter().rev() {
+    // Build the BTreeMap keyed by romaji key length
+    for (romaji, kana) in ROMAJI_TO_KANA.iter() {
+        romaji_to_kana_by_key_size
+            .entry(romaji.chars().count())
+            .or_insert_with(HashMap::new)
+            .insert(romaji, kana);
+    }
+
+    // Reverse the BTreeMap iterator to effectively start from the largest romaji key
+    for (_k, v) in romaji_to_kana_by_key_size.iter().rev() {
         for (romaji, kana) in v {
             result = result.replace(romaji, kana);
         }
@@ -100,19 +99,3 @@ fn replace_by_size(s: &str) -> String {
 
     result
 }
-
-fn romaji_to_kana() -> BTreeMap<usize, HashMap<&'static str, &'static str>> {
-    // BTreeMap to allow for a sorted lookup by key length
-    let mut map: BTreeMap<usize, HashMap<&str, &str>> = BTreeMap::new();
-
-    for (k, v) in ROMAJI_TO_KANA.iter() {
-        group_by_length(&mut map, k, v);
-    }
-
-    map
-}
-
-fn group_by_length<'a>(map: &mut BTreeMap<usize, HashMap<&'a str, &'a str>>, romaji: &'a str, kana: &'a str) {
-    map.entry(romaji.chars().count()).or_insert_with(HashMap::new).insert(romaji, kana);
-}
-
